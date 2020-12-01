8f282c79efea760202aa4d662635ab88c46d421d
Murat Can Kuru <49204501+muratcankuruoffical@users.noreply.github.com>
2020-12-01 19:24:02 +0300

    UserController.php and Vue install
    UserController.php was created. Register and Login functions are defined. Also vue install

---
+++ realestate/app/Http/Controllers/API/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+
+class UserController extends Controller
+{
+    //
+    public function login(Request $request) {
+        $credentials = $request->only('email', 'password');
+        try {
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return response()->json(['error' => 'invalid creadentials'], 400);
+            }
+        }catch (JWTException $e) {
+            return response()->json(['error' => 'could not create token'], 500);
+        }
+        return response()->json(compact('token'));
+    }
+    public function register(Request $request) {
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6|confirmed',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toJson(),400);
+        }
+
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password')),
+        ]);
+        $token = JWTAuth::fromUser($user);
+        return response()->json(compact('user','token'),201);
+    }
+
+
+
+}
--- realestate/routes/api.php
+++ realestate/routes/api.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('register', [\App\Http\Controllers\API\UserController::class, 'register']);
+Route::post('login', [\App\Http\Controllers\API\UserController::class, 'login']);
